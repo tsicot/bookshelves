007b022c53ae2bd2316858dccb834864b7c87456
Timothée Sicot <timothee.sicot@gmail.com>
2019-02-25 14:52:39 +0100

    Hidding google credentials

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import {Component} from '@angular/core';
+import * as firebase from 'firebase';
+import {FirebaseAuthService} from './services/firebase-auth.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  constructor(private firebaseAuth: FirebaseAuthService) {
+    const config = firebaseAuth.getAuthData();
+
+    firebase.initializeApp(config);
+  }
+}
