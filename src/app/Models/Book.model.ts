06a3a9ff4393dad27c38695c7fd3330ebba64f43
Timothée Sicot <timothee.sicot@gmail.com>
2019-02-25 14:36:05 +0100

    Basic structure and auth

---
+++ src/app/Models/Book.model.ts
@@ -0,0 +1,5 @@
+export class Book {
+  photo: string;
+
+  constructor(public titre: string, public auteur: string) {}
+}
---
+++ src/app/services/auth-guard.service.ts
@@ -0,0 +1,32 @@
+import {Injectable} from '@angular/core';
+import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot} from '@angular/router';
+import {Observable} from 'rxjs';
+import * as firebase from 'firebase';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuardService implements CanActivate {
+
+  constructor(private router: Router) {
+  }
+
+  canActivate(route: ActivatedRouteSnapshot,
+              state: RouterStateSnapshot):
+    Observable<boolean> | Promise<boolean> | boolean {
+    return new Promise(
+      ((resolve, reject) => {
+        firebase.auth().onAuthStateChanged(
+          (user) => {
+            if (user) {
+              resolve(true);
+            } else {
+              this.router.navigate(['/auth', 'signin']);
+              resolve(false);
+            }
+          }
+        );
+      })
+    );
+  }
+}
