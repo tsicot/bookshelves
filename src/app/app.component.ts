085f8185becf05b7c3bd5521bd557d26afa2838c
Timothée Sicot <timothee.sicot@gmail.com>
2019-02-25 14:36:05 +0100

    Basic structure and auth

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'bookshelves';
+  constructor() {
+    const config = {
+      apiKey: 'AIzaSyB5to5lbCbIXEVWqIIsV-vTDXPRPDDNGqc',
+      authDomain: 'bookshelves-fdea2.firebaseapp.com',
+      databaseURL: 'https://bookshelves-fdea2.firebaseio.com',
+      projectId: 'bookshelves-fdea2',
+      storageBucket: 'bookshelves-fdea2.appspot.com',
+      messagingSenderId: '108673099194'
+    };
+    firebase.initializeApp(config);
+  }
 }
